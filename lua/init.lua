d80e041d86dffa8cb2f554517b0117b131c9b13e
MikeT3ch <124947421+MikeT3ch@users.noreply.github.com>
2025-10-26 22:12:21 -0500

    Added auxiliar func get_notif_data

--- lua/init.lua
+++ lua/init.lua
@@ -1,1 +1,19 @@
 local M = {}
+
+local config = {
+  default_frames = { "🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "🟤", "⚫" },
+  default_speed = 150
+}
+
+function M.setup(opts)
+  config = vim.tbl_extend("force", config, opts or {}) -- dont know how to deal with vim. stuff
+end
+
+local notif_data = {}
+
+local function get_notif_data(key)
+  if not notif_data[key] then
+    notif_data[key] = {}
+  end
+  return notif_data[key]
+end
