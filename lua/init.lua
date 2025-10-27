12dfa02af1ee3303754682dc0bb924b46090eaf2
MikeT3ch <124947421+MikeT3ch@users.noreply.github.com>
2025-10-26 22:26:08 -0500

    added initial process of notifications

--- lua/init.lua
+++ lua/init.lua
@@ -10,14 +10,14 @@ function M.setup(opts)
   config = vim.tbl_extend("force", config, opts or {}) -- dont know how to deal with vim.thing stuff
 end
 
-function M.notif_static(msg, level, opts)
+function M.progress_static(msg, level, opts)
   opts = opts or {}
   vim.notify(msg, level or vim.log.levels.INFO, opts)
 end
 
 local notif_data = {}
 
-local function get_notif_data(key)
+local function get_notify_data(key)
   if not notif_data[key] then
     notif_data[key] = {}
   end
@@ -25,7 +25,7 @@ local function get_notif_data(key)
 end
 
 local function update_spinner(key)
-  local data = get_notif_data(key)
+  local data = get_notify_data(key)
   if data.spinner and data.spinner_frames then
     local new_spinner = (data.spinner % #data.spinner_frames) + 1
     data.spinner = new_spinner
@@ -42,5 +42,26 @@ local function update_spinner(key)
   end
 end
 
+function M.progress_progressive(key, title, opts)
+  opts = opts or {}
+  local spinner_frames = opts.spinner or config.default_frames
+  local speed = opts.speed or config.default_speed
+  local data = get_notify_data(key)
+  data.title = title
+  data.spinner_frames = spinner_frames
+  data.speed = speed
+
+  return {
+    begin = function()
+
+    end,
+    report = function()
+
+    end,
+    done = function()
+
+    end
+  }
+end
 
 return M
